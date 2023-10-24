90e8d4363bb7850eada621b6566cf0e4e1355bb7
fred-gutierrez <fredsg22@hotmail.com>
2023-10-23 18:09:20 -0600

    Added localStorage save on the darkMode function

--- src/lib/DarkModeSwitch.svelte
+++ src/lib/DarkModeSwitch.svelte
@@ -1,5 +1,13 @@
 <script>
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+
   let darkMode = false;
+
+  if (browser) {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    darkMode = storedDarkMode ? JSON.parse(storedDarkMode) : false;
+  }
 
   function toggleDarkMode() {
     darkMode = !darkMode;
@@ -9,7 +17,17 @@
     } else {
       document.documentElement.classList.remove("dark");
     }
+
+    if (browser) {
+      localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    }
   }
+
+  onMount(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    }
+  });
 </script>
 
 <button on:click={toggleDarkMode}>
